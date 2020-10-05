df484278af3ce1e7ad82844aa8c8387e662102ec
Yousef Roshandel <yousef.r1999@gmail.com>
2020-10-05 18:34:30 +0330

    configure colors class

---
+++ lib/theme/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+Color darkBackgroundColor = const Color(0xff0A0A14);
+Color darkGreyColor = const Color(0xff14151D);
+Color lightGrayColor = const Color(0xff6B6B6B);
+Color whiteColor = const Color(0xffE6E7EF);
+Color navyBlueColor = const Color(0xff191924);
+Color independenceBlueColor = const Color(0xff2B2E43);
+Color blueColor = const Color(0xff3269FF);
+Color greenColor = const Color(0xff00CD69);
+Color redColor = const Color(0xffF14147);
+Color yellowColor = const Color(0xffEBAD06);
+
+final appTheme = ThemeData(
+  primaryColor: darkBackgroundColor,
+  primarySwatch: Colors.blue,
+);
